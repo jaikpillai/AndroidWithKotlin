5c898e36f7a7dd4dea1d06fa222d6f613046207a
Jai <jaipilllai@gmail.com>
2019-05-22 14:31:39 +0530

    Double Dice

--- app/src/main/java/com/jai/diceroller/MainActivity.kt
+++ app/src/main/java/com/jai/diceroller/MainActivity.kt
@@ -10,13 +10,15 @@ import kotlinx.android.synthetic.main.activity_main.*
 import java.util.*
 
 class MainActivity : AppCompatActivity() {
-    lateinit var diceImage: ImageView
+    lateinit var diceImage1: ImageView
+    lateinit var diceImage2: ImageView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         val rollButton: Button = findViewById(R.id.roll_button)
-       diceImage = findViewById(R.id.dice_image)
+        diceImage1 = findViewById(R.id.dice_image1)
+        diceImage2 = findViewById(R.id.dice_image2)
         val resultText: TextView = findViewById(R.id.result_text)
         rollButton.setOnClickListener{
             rollDice()
@@ -26,10 +28,20 @@ class MainActivity : AppCompatActivity() {
 
     fun rollDice(){
 
-        val randomInt = Random().nextInt(6)+1
-        result_text.text = randomInt.toString()
+        val randomInt1 = Random().nextInt(6)+1
+        val randomInt2 = Random().nextInt(6)+1
+        result_text.text = (randomInt1+randomInt2).toString()
 
-        var drawableResource = when(randomInt){
+        var drawableResource1 = when(randomInt1){
+            1->R.drawable.dice_1
+            2->R.drawable.dice_2
+            3->R.drawable.dice_3
+            4->R.drawable.dice_4
+            5->R.drawable.dice_5
+            else->R.drawable.dice_6
+
+        }
+        var drawableResource2 = when(randomInt2){
             1->R.drawable.dice_1
             2->R.drawable.dice_2
             3->R.drawable.dice_3
@@ -39,7 +51,9 @@ class MainActivity : AppCompatActivity() {
 
         }
 
-        dice_image.setImageResource(drawableResource)
+
+        dice_image1.setImageResource(drawableResource1)
+        dice_image2.setImageResource(drawableResource2)
 
     }
 }
